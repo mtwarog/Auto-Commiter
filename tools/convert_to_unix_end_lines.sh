f2cbd0d696968f8858b38e22bdba31c196da8dfe
Maciej Twarog <maciej.twarog1@gmail.com>
2019-09-15 09:15:07 +0200

    Improve line-ending converter to convert only recently added commit

--- tools/convert_to_unix_end_lines.sh
+++ tools/convert_to_unix_end_lines.sh
@@ -1,1 +1,3 @@
-echo -e "$(find ../data/ -name '*.txt')\n$(find ../data/ -name '*.md')" | xargs dos2unix
+# Converts windows line-endings to unix ones
+# USAGE: ./convert_to_unix_end_lines.sh COMMIT_DIR_NAME
+echo -e "$(find ../data/$1 -name '*.txt')\n$(find ../data/$1 -name '*.md')" | xargs dos2unix
